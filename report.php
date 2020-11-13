fa31568ecaa5500f4b475e626099d794d95838d4
Rachel MARTIN <r.shell.dev@gmail.com>
2020-11-13 12:33:27 +0100

    premier commit, base de plugin report

---
+++ report.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class quiz_gradedbycategory_report extends quiz_default_report {
+    public function display($cm, $course, $quiz) {
+        // Generate and display the report, or
+        // other functionality.
+        // see mod/quiz/report/attemptsreport.php
+        // and see mod/quiz/report/reportlib.php
+    }
+}
